b224b12791a4ca6f5c8cd6662deb695574e91b86
Jeff Vacca <jeffvacca@gmail.com>
2024-03-08 09:33:46 -0500

    config

---
+++ config/site.ts
@@ -0,0 +1,46 @@
+export type SiteConfig = typeof siteConfig;
+
+export const siteConfig = {
+	name: "Jeff Vacca",
+	description: "Senior Frontend Engineer",
+	navItems: [
+		{
+			label: "Home",
+			href: "/",
+		},
+		{
+			label: "Work",
+			href: "/work",
+		},
+		{
+			label: "About",
+			href: "/about",
+		},
+		{
+			label: "Skills",
+			href: "/skills",
+		}
+	],
+	navMenuItems: [
+		{
+			label: "Home",
+			href: "/",
+		},
+		{
+			label: "Work",
+			href: "/work",
+		},
+		{
+			label: "About",
+			href: "/about",
+		},
+		{
+			label: "Skills",
+			href: "/skills",
+		}
+	],
+	links: {
+		linkedIn: "https://www.linkedin.com/in/jeff-vacca-1678247a/",
+		github: "https://github.com/jeffvacca"
+	},
+};
